8f64ae975330d7daa3e9b07df71e4e841a2191ce
Davi-Zimmer <davizimmerv5@gmail.com>
2026-06-04 21:51:46 -0300

    Map Creator Selection

--- Game/Core/MapCreator.cs
+++ Game/Core/MapCreator.cs
@@ -1,8 +1,47 @@
+using System.Numerics;
 using Game.Objects;
 using Game.Rendering;
+using Game.World.Entity;
+using Game.World.Entity.Enemy;
+using Game.World.Tile;
 using Raylib_cs;
 
 namespace Game.Core;
+
+
+public class PaletteItem {
+    public GameObject gameObject = GameObject.None;
+    public Type classObject;
+    public List<Rectangle> previewSprites;
+
+    public PaletteItem( GameObject o, Type classObj, List<Rectangle> preview ) {
+        gameObject     = o;
+        classObject    = classObj;
+        previewSprites = preview;
+    }
+
+}
+
+public class OrganizedPeletteItem {
+    public Rectangle pos;
+    public Rectangle sprite;
+    public Type classObject;
+    public GameObject gameObject;
+
+    public OrganizedPeletteItem(
+        Rectangle pos,
+        Rectangle sprite,
+        Type classObject,
+        GameObject gameObject
+    ) {
+        this.pos         = pos;
+        this.sprite      = sprite;
+        this.classObject = classObject;
+        this.gameObject  = gameObject;
+    }
+
+
+}
 
 
 public class MapCreator {
@@ -12,13 +51,84 @@ public class MapCreator {
     public bool open = false;
     public bool itemsInterface = false;
 
-    private List< WorldObject > map = [];
+    private List<WorldObject> map = [];
 
+    private List<OrganizedPeletteItem> organizedItems;
+
+    private OrganizedPeletteItem? selectedItem = null; 
 
     private int z = 0;
 
     public MapCreator( Main game ) {
+
         this.game = game;
+
+        organizedItems = organizeItems();
+
+    }
+
+    private List<OrganizedPeletteItem> organizeItems() {
+
+        List<PaletteItem> items = new() {
+
+            new PaletteItem(
+                GameObject.Player,
+                typeof( Player ),
+                Sprites.GetRects( Sprites.Player )
+            ),
+
+            new PaletteItem (
+                GameObject.Slime,
+                typeof( Slime ),
+                Sprites.GetRects( Sprites.Slime )
+            ),
+
+            new PaletteItem (
+                GameObject.Grass,
+                typeof( GenericTile ),
+                Sprites.GetRects( Sprites.Grass )
+            ),
+
+            new PaletteItem (
+                GameObject.StoneWall,
+                typeof( GenericTile ),
+                Sprites.GetRects( Sprites.StoneWall )
+            ),
+
+            new PaletteItem (
+                GameObject.CrackedStoneWall,
+                typeof( GenericTile ),
+                Sprites.GetRects( Sprites.CrackedStoneWall )
+            ),
+
+        };
+        
+        List<OrganizedPeletteItem> list = [];
+
+        int size = 50;
+        int space = 10;
+
+        for ( int index = 0; index < items.Count; index++ ) {
+            
+            PaletteItem item = items[ index ];
+            
+            for( int x = 0; x < item.previewSprites.Count; x++ ) {
+
+                int posX = x     * size + space * x;
+                int posY = index * size + space * index;
+
+                list.Add( new OrganizedPeletteItem(
+                    new Rectangle( posX, posY, size, size ),
+                    item.previewSprites[ x ],
+                    item.classObject,
+                    item.gameObject
+                ));
+
+            }
+
+        }
+
+        return list;
     }
 
     public void events( float delta, ref Camera2D cam ) {
@@ -40,6 +150,59 @@ public class MapCreator {
         if( Raylib.IsKeyPressed( KeyboardKey.Down ) ) z--; else
         if( Raylib.IsKeyPressed( KeyboardKey.Up   ) ) z++;
 
+        if( Raylib.IsMouseButtonPressed( MouseButton.Left ) ) leftClick( Raylib.GetMousePosition() );
+
+
+    }
+
+    private bool inside( Vector2 v, Rectangle r ) {
+        return (
+            v.X > r.X &&
+            v.Y > r.Y &&
+            v.X < r.X + r.Width &&
+            v.Y < r.Y + r.Height 
+        );
+    }
+
+    private void leftClick( Vector2 vec ) {
+
+        foreach( var item in organizedItems ) {
+
+            if( inside( vec, item.pos ) ) {
+                
+                selectedItem = item;
+                
+                if( selectedItem != null ) Console.WriteLine( selectedItem.gameObject );
+
+                return;
+
+            }
+        
+        }
+
+
+    }
+
+    public void drawInterface( ref Camera2D cam, float delta, Texture2D spriteSheet ) {
+
+        Raylib.DrawRectangle( 0, 0, (int)game.getInnerWidth(), (int)game.getInnerHeight(), Color.Black );
+
+        foreach( var item in organizedItems ) {
+        
+            Raylib.DrawRectangle( (int)item.pos.X, (int)item.pos.Y, (int)item.pos.Width, (int)item.pos.Height, Color.Blue );
+
+            var vec = new Vector2( 0, 0 );
+        
+            Raylib.DrawTexturePro( 
+                spriteSheet,
+                item.sprite,
+                item.pos,
+                vec,
+                0,
+                Color.White
+            );
+
+        }
 
     }
 
@@ -49,8 +212,7 @@ public class MapCreator {
 
         if( itemsInterface ) {
 
-            Raylib.DrawRectangle( 0, 0, (int)game.getInnerWidth(), (int)game.getInnerHeight(), Color.Red );
-            
+            drawInterface( ref cam, delta, spriteSheet );
 
             return;
         }
@@ -77,8 +239,6 @@ public class MapCreator {
 
 
     }
-
-
 
     private void toggleItemsInterface(){ itemsInterface = !itemsInterface; } 
     public void toggleMapCreation() { open = !open; }
--- Game/Rendering/Sprites.cs
+++ Game/Rendering/Sprites.cs
@@ -1,39 +1,43 @@
+using System.Reflection;
 using Game.World.Entity;
 using Raylib_cs;
 
 namespace Game.Rendering;
 
-
-
 public class PlayerSprites {
-    public Rectangle Down  => new Rectangle( 0,   0, 27, 36 );
-    public Rectangle Left  => new Rectangle( 0,  37, 27, 36 );
-    public Rectangle Right => new Rectangle( 0,  75, 27, 36 );
-    public Rectangle Up    => new Rectangle( 0, 115, 27, 36 );
+    public readonly Rectangle Down  = new( 0,   0, 27, 36 );
+    public readonly Rectangle Left  = new( 0,  37, 27, 36 );
+    public readonly Rectangle Right = new( 0,  75, 27, 36 );
+    public readonly Rectangle Up    = new( 0, 115, 27, 36 );
 }
 
 public class SlimeSprites {
-    public Rectangle JumpRight => new Rectangle( 1,  158, 48, 42 );
+    public Rectangle JumpRight = new( 1,  158, 48, 42 );
 
 }
 
 public class GrassSprites {
-    public Rectangle Middle => new Rectangle( 151, 1, 32, 32 );
+    public Rectangle Middle = new( 151, 1, 32, 32 );
     
 }
 
 
 public class StoneWallSprites {
-    public Rectangle Middle => new Rectangle( 184, 1, 32, 32 );
+    public Rectangle Middle = new( 184, 1, 32, 32 );
     
 }
 
 public class CrackedStoneWallSprites {
-    public Rectangle Middle => new Rectangle( 151, 1, 32, 32 );
+    public Rectangle Middle = new( 217, 34, 32, 32 );
     
 }
 
-public abstract class Sprites {
+public record SpritePreview(
+    string Name,
+    Rectangle Rect
+);
+
+public  class Sprites {
 
     public static readonly PlayerSprites Player = new();
     public static readonly SlimeSprites  Slime  = new();
@@ -43,6 +47,29 @@ public abstract class Sprites {
     public static readonly StoneWallSprites        StoneWall         = new();
     public static readonly CrackedStoneWallSprites CrackedStoneWall  = new();
 
+    public static List<Rectangle> GetRects( object obj ) {
+
+        var resultado = new List<Rectangle>();
+
+        var campos = obj.GetType().GetFields(
+            BindingFlags.Instance |
+            BindingFlags.Public |
+            BindingFlags.NonPublic
+        );
+
+        foreach( var campo in campos ) {
+            
+            if (campo.FieldType == typeof(Rectangle)) {
+
+                resultado.Add((Rectangle)campo.GetValue(obj)!);
+
+            }
+
+        }
+
+        return resultado;
+
+    }
 
 }
 
